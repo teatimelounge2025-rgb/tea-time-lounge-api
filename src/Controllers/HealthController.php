a213c6e9ddb8874b954ed78f789df6c6218a6181
Alex <info@jibodev.eu>
2026-03-12 19:49:34 +0100

    Add index method to HealthController

--- src/Controllers/HealthController.php
+++ src/Controllers/HealthController.php
@@ -3,11 +3,9 @@ declare(strict_types=1);
 
 namespace TeaTimeLounge\ApiGateway\Controllers;
 
-use TeaTimeLounge\ApiGateway\Http\Request;
-
 final class HealthController
 {
-    public function index(Request $request): array
+    public function index(array $params = []): array
     {
         return [
             'status' => 200,
