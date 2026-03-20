87ef55d6dc2346b2ad0964f6a07cdb231d8d324a
Alex <info@jibodev.eu>
2026-03-20 19:05:06 +0100

    Add lead email follow up generation endpoint with OpenAI

--- src/Controllers/LeadImportController.php
+++ src/Controllers/LeadImportController.php
@@ -664,7 +664,7 @@ IMPORTANT:
 - If data is missing, stay generic
 
 Core rules:
-- Keep emails between 150-190 words
+- Keep emails between 150-180 words
 - Write like a real person, not marketing copy
 - Do NOT invent facts about the lead
 - Do NOT over-explain the technology
